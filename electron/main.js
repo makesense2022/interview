84f127b9acf53775f939aa97493381bc3b0bb71d
fe-hall <11434555+fe-hall@user.noreply.gitee.com>
2025-11-28 14:47:32 +0800

    feat: add electron project

---
+++ electron/main.js
@@ -0,0 +1,40 @@
+import { app, BrowserWindow, ipcMain } from 'electron'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+let win
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 1024,
+    height: 768,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
+      sandbox: true
+    }
+  })
+
+  win.loadFile(path.join(__dirname, 'renderer', 'index.html'))
+
+  win.on('closed', () => {
+    win = null
+  })
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow()
+})
+
+// minimal IPC
+ipcMain.handle('ping', async () => 'pong')
